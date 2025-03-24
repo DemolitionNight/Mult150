d766d2b990120e3c99fc2c4bfb54d939c9f98992
DemolitionNight <angibata@yahoo.com>
2025-03-23 23:39:12 -0700

    Hour 7 Part 2
    Second half of hour 7 scripting 1

---
+++ ABatakovic_Hour7_P2/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float health = 1004f;
+        float poisonDamange = 125.5f;
+
+        Debug.Log(health);
+
+        while (health > 0)
+        {
+            health -= poisonDamange;
+
+            Debug.Log(health);
+        }
+
+        if (health <= 0)
+        {
+            Debug.Log("Player has been unalived!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
