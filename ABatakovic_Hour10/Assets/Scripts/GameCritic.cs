15648c0532d82af7f31884382cf9c8ebbc026af0
DemolitionNight <angibata@yahoo.com>
2025-04-28 02:49:59 -0700

    Hour 10
    Chaos Ball

---
+++ ABatakovic_Hour10/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,23 @@
+/* 
+ Improving the Game:
+
+1) The game is hard in that it is hard to actually purposfully hit a ball since they are moving around really fast, but it's easy in that a lot of the time the balls randomly hit the goals
+on their own without you needing to put in any effort.
+
+2) Slowing down the balls and increasing the players walk speed might help make the game a bit easier. Putting more obstacles in the arena could make the game harder but more interesting.
+Increasing bumper size and giving it a different shape could help make it easier to actually hit the balls.
+
+3) Adding some sort of theme to the game and/or adding another game mechanic, for instance the chaos balls injure the player, or the player also needs to put the chaos balls into their own
+goal would make the game more interesting and give it a wow factor.
+
+4) It's pretty tedious trying to actually hit the ball with the bumper the way it is so the bumper itself could probably use some work, like changing it's size and shape. The fun parts are 
+when you can sucessfully redirect the ball into the goal and then youre like yeah, thats right, I did that.
+
+Sugesstions Implemented:
+
+
+
+
+
+
+*/
