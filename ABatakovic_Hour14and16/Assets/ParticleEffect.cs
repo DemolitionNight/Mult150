d63d4e1b96d0608a5dc2f8039024287891312669
DemolitionNight <angibata@yahoo.com>
2025-05-15 17:00:49 -0700

    Hour 14 and 16
    UI and Particle Effects

---
+++ ABatakovic_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,7 @@
+/*
+ 1) doubled the duration and start lifetime from 5 to 10.
+ 2) I changed the start speed to 15, now with this change and the previous one the particals travel a lot higher into the air than before.
+ 3) For start size I used the curve editor to make them start big and get smaller as their life nears the end.
+ 4) I changed the start color so it is a gradient from red to yellow.
+ 5) Under Shape, I changed the radius and angle for the cone emitter.
+*/
---
+++ ABatakovic_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,6 @@
+/*
+I gave my game a title and a subtitle/simple desccription and changed the font, size, color, and placement/anchors for them.
+I changed the color of my button and made it so only the boarder shows while the inside is transparanet. 
+I changed the font style, size, and color of the text inside the button.
+I also gave the button a different color when highlighted/pressed, it's interesting to see how the color i picked for the button mixes with the transition color.
+ */
